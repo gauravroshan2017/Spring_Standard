642c44220c0e8306440f24083013e3d8ec287a69
Awasthi <Awasthi@Awasthi>
2017-07-28 12:31:04 +0530

    Initial ssetup of project in process

---
+++ IIHT/CartSpring/src/main/java/com/gaurav/dao/OrderDAO.java
@@ -0,0 +1,23 @@
+package com.gaurav.dao;
+
+import java.util.List;
+
+import com.gaurav.model.CartInfo;
+import com.gaurav.model.OrderDetailInfo;
+import com.gaurav.model.OrderInfo;
+import com.gaurav.model.PaginationResult;
+
+
+ 
+public interface OrderDAO {
+ 
+    public void saveOrder(CartInfo cartInfo);
+ 
+    public PaginationResult<OrderInfo> listOrderInfo(int page,
+            int maxResult, int maxNavigationPage);
+    
+    public OrderInfo getOrderInfo(String orderId);
+    
+    public List<OrderDetailInfo> listOrderDetailInfos(String orderId);
+ 
+}
