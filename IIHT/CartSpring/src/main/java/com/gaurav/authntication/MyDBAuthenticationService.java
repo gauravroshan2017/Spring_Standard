a279e053ae9386eb15eeffece1edeb13332bcb0f
Awasthi <Awasthi@Awasthi>
2017-07-28 12:32:44 +0530

    Initial setting in process

--- IIHT/CartSpring/src/main/java/com/gaurav/authntication/MyDBAuthenticationService.java
+++ IIHT/CartSpring/src/main/java/com/gaurav/authntication/MyDBAuthenticationService.java
@@ -4,6 +4,7 @@ import java.util.List;
  // http://o7planning.org/en/10605/create-a-java-shopping-cart-web-application-using-spring-mvc-and-hibernate step 11
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
+
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
